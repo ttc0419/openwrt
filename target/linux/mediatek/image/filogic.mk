44c469f780618cdd85461c0a114a2040c48db7fc
Tianchen Tang <ttc0419@outlook.com>
2026-06-17 14:24:24 +0800

    kernel: add chipup xs2184 pse i2c driver
    Reverse engineered from https://github.com/BeeconMini/openwrt/blob/25.12.2/package/kernel/xs2184/src/xs2184.ko
    
    Signed-off-by: Tianchen Tang <ttc0419@outlook.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -842,7 +842,7 @@ define Device/beeconmini_seed-ac1
   DEVICE_MODEL := SEED AC1
   DEVICE_DTS := mt7981b-beeconmini-seed-ac1
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := -wpad-basic-mbedtls kmod-i2c-gpio kmod-sfp kmod-usb3 kmod-fs-f2fs mkf2fs
+  DEVICE_PACKAGES := -wpad-basic-mbedtls kmod-i2c-gpio kmod-sfp kmod-usb3 kmod-fs-f2fs kmod-xs2184 mkf2fs
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += beeconmini_seed-ac1
